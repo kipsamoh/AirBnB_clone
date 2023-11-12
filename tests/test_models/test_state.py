8a5e55ba954e3cc7d06ebc5412a4029714b60f1b
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-11-12 19:44:53 +0300

    test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -10,30 +10,30 @@ import datetime
 class TestState(unittest.TestCase):
     """ State class instances and methods test """
 
-    s = State()
+    _s = State()
 
     def test_class_exists(self):
         """checks if class exists"""
-        res = "<class 'models.state.State'>"
-        self.assertEqual(str(type(self.s)), res)
+        _res = "<class 'models.state.State'>"
+        self.assertEqual(str(type(self._s)), _res)
 
     def test_user_inheritance(self):
         """checks whether State is a subclass of BaseModel"""
-        self.assertIsInstance(self.s, State)
+        self.assertIsInstance(self._s, State)
 
     def testHasAttributes(self):
         """check if attributes exist"""
-        self.assertTrue(hasattr(self.s, 'name'))
-        self.assertTrue(hasattr(self.s, 'id'))
-        self.assertTrue(hasattr(self.s, 'created_at'))
-        self.assertTrue(hasattr(self.s, 'updated_at'))
+        self.assertTrue(hasattr(self._s, 'name'))
+        self.assertTrue(hasattr(self._s, 'id'))
+        self.assertTrue(hasattr(self._s, 'created_at'))
+        self.assertTrue(hasattr(self._s, 'updated_at'))
 
     def test_types(self):
         """tests if the type of the attribute is the correct one"""
-        self.assertIsInstance(self.s.name, str)
-        self.assertIsInstance(self.s.id, str)
-        self.assertIsInstance(self.s.created_at, datetime.datetime)
-        self.assertIsInstance(self.s.updated_at, datetime.datetime)
+        self.assertIsInstance(self._s.name, str)
+        self.assertIsInstance(self._s.id, str)
+        self.assertIsInstance(self._s.created_at, datetime.datetime)
+        self.assertIsInstance(self._s.updated_at, datetime.datetime)
 
 if __name__ == '__main__':
     unittest.main()
