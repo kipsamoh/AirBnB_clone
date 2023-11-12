da5b5251c83b15bdada664350ee6e7cca34b86cf
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-11-12 20:12:47 +0300

    generate-authors.sh

--- generate-authors.sh
+++ generate-authors.sh
@@ -8,14 +8,13 @@ SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOTDIR="$(cd "${SCRIPTDIR}/.." && pwd)"
 
 # Change to the Git repository directory
-cd "${ROOTDIR}/AirBnB_clone"
+cd "${ROOTDIR}/printf"
 
 # Continue script execution even if some commands fail
 set -x
 
 # Generate AUTHORS file, placing kipsamoh <kipsamoh@gmail.com> as the first contributor
-cat > "${ROOTDIR}/AirBnB_clone/AUTHORS" <<- EOF
-    # This file lists all contributors to the repository.
-
-    $(git log --format='%aN <%aE>' | grep -v -E '^(kipsamoh <133492659\+kipsamoh@users\.noreply\.github\.com>|kipsamoh <kipsamoh@gmail\.com>)$' | LC_ALL=C.UTF-8 sort -uf | sed 's/ /_/g'
-EOF
+{
+  printf "%-4s %-30s %-50s\n" "# This file lists all contributors to the repository." "" ""
+  git log --format='%aN <%aE>' | grep -v -E '^(kipsamoh <133492659\+kipsamoh@users\.noreply\.github\.com>|>)$' | LC_ALL=C.UTF-8 sort -uf | sed 's/ / /g'
+} > "${ROOTDIR}/printf/AUTHORS"
