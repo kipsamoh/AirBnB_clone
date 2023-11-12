4e34346e8959d13ba5a21eebe595569a3b0c9a95
Daniel Chebet <133492659+kipsamoh@users.noreply.github.com>
2023-11-12 13:07:28 +0300

    test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Unittest for review.py
+review.py unittests
 """
 import unittest
 from models.review import Review
@@ -8,21 +8,21 @@ import datetime
 
 
 class TestReview(unittest.TestCase):
-    """Tests instances and methods from Review class"""
+    """Review class instances and methods tests"""
 
     r = Review()
 
     def test_class_exists(self):
-        """tests if class exists"""
+        """checks if class exists"""
         res = "<class 'models.review.Review'>"
         self.assertEqual(str(type(self.r)), res)
 
     def test_user_inheritance(self):
-        """test if Review is a subclass of BaseModel"""
+        """checks if Review is a subclass of BaseModel"""
         self.assertIsInstance(self.r, Review)
 
     def testHasAttributes(self):
-        """verify if attributes exist"""
+        """check if attributes exist"""
         self.assertTrue(hasattr(self.r, 'place_id'))
         self.assertTrue(hasattr(self.r, 'user_id'))
         self.assertTrue(hasattr(self.r, 'text'))
@@ -31,7 +31,7 @@ class TestReview(unittest.TestCase):
         self.assertTrue(hasattr(self.r, 'updated_at'))
 
     def test_types(self):
-        """tests if the type of the attribute is the correct one"""
+        """checks if attribute is the correct one"""
         self.assertIsInstance(self.r.place_id, str)
         self.assertIsInstance(self.r.user_id, str)
         self.assertIsInstance(self.r.text, str)
