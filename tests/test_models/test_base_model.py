c236939162a9dd538dbdbf94e5dedc4200984475
Daniel Chebet <133492659+kipsamoh@users.noreply.github.com>
2023-11-12 12:48:19 +0300

    test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module of Unittests """
+""" Unittests module """
 import unittest
 from models.base_model import BaseModel
 import os
@@ -9,38 +9,38 @@ import datetime
 
 
 class BaseModelTests(unittest.TestCase):
-    """ Suite of Console Tests """
+    """  Console Tests suite """
 
-    my_model = BaseModel()
+    _mymodel = BaseModel()
 
     def testBaseModel1(self):
-        """ Test attributes value of a BaseModel instance """
+        """  BaseModel instance value tests """
 
-        self.my_model.name = "Holberton"
-        self.my_model.my_number = 89
-        self.my_model.save()
-        my_model_json = self.my_model.to_dict()
+        self._mymodel.name = "ALX"
+        self._mymodel.my_number = 89
+        self._mymodel.save()
+        _mymodel_json = self._mymodel.to_dict()
 
-        self.assertEqual(self.my_model.name, my_model_json['name'])
-        self.assertEqual(self.my_model.my_number, my_model_json['my_number'])
-        self.assertEqual('BaseModel', my_model_json['__class__'])
-        self.assertEqual(self.my_model.id, my_model_json['id'])
+        self.assertEqual(self._mymodel.name, _mymodel_json['name'])
+        self.assertEqual(self._mymodel.my_number, _mymodel_json['my_number'])
+        self.assertEqual('BaseModel', _mymodel_json['__class__'])
+        self.assertEqual(self._mymodel.id, _mymodel_json['id'])
 
     def testSave(self):
-        """ Checks if save method updates the public instance instance
+        """ validates whether public instance instance modules 
         attribute updated_at """
-        self.my_model.first_name = "First"
-        self.my_model.save()
+        self._mymodel.first_name = "First"
+        self._mymodel.save()
 
-        self.assertIsInstance(self.my_model.id, str)
-        self.assertIsInstance(self.my_model.created_at, datetime.datetime)
-        self.assertIsInstance(self.my_model.updated_at, datetime.datetime)
+        self.assertIsInstance(self._mymodel.id, str)
+        self.assertIsInstance(self._mymodel.created_at, datetime.datetime)
+        self.assertIsInstance(self._mymodel.updated_at, datetime.datetime)
 
-        first_dict = self.my_model.to_dict()
+        first_dict = self._mymodel.to_dict()
 
-        self.my_model.first_name = "Second"
-        self.my_model.save()
-        sec_dict = self.my_model.to_dict()
+        self._mymodel.first_name = "Second"
+        self._mymodel.save()
+        sec_dict = self._mymodel.to_dict()
 
         self.assertEqual(first_dict['created_at'], sec_dict['created_at'])
         self.assertNotEqual(first_dict['updated_at'], sec_dict['updated_at'])
